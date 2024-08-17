1eed92860b2cbcbafbb6e859938c00587b2dd135
kaji-sugiura <kaji.sugiura@gmail.com>
2024-08-17 15:03:20 +0900

    file_manipulatorの実装

---
+++ file_converter.py
@@ -0,0 +1,14 @@
+import markdown
+import sys
+
+markdown_file = "markdown.md"
+output_file_path = "out.html"
+
+markdown_contents = ""
+
+with open(markdown_file) as f:
+    markdown_contents = f.read()
+    
+with open(output_file_path, "w") as f:
+    html = markdown.markdown(markdown_contents)
+    f.write(html)
---
+++ markdown.md
@@ -0,0 +1,11 @@
+# SAMPLE
+
+## 見出し 2
+
+- sample.
+- sample.
+
+### 見出し 3
+
+- sample
+- sample
